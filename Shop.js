f292bebce91558f351bf0eca614059507973aa73
zaramahroyan <88336080+zaramahroyan@users.noreply.github.com>
2024-02-20 01:04:06 +0500

    Update Shop.js
    Signed-off-by: zaramahroyan <88336080+zaramahroyan@users.noreply.github.com>

--- Shop.js
+++ Shop.js
@@ -1,17 +1,1 @@
-const mongoose = require('mongoose');
 
-const ShopSchema = new mongoose.Schema({
-    prodID: String,
-    name: String,
-    mfgDate: Date,
-    expDate: Date,
-    user: {type: mongoose.SchemaTypes.ObjectId, ref: 'Users'},
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-});
-
-const Shop = mongoose.model('Shop', ShopSchema);
-
-module.exports = Shop;
